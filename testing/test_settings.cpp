cb362503d001bae84b5fc99e87fa9b39e1e44f66
Moritz Trieu <ninj24o@gmail.com>
2024-10-26 22:12:19 +0200

    Completed parser testing

--- testing/test_settings.cpp
+++ testing/test_settings.cpp
@@ -1,12 +1,44 @@
 #include <gtest/gtest.h>
 #include "settings/settings.h"
 
-TEST(SettingsTest, RemoveStartWhiteSpace)
+class SettingsTest : public testing::Test
 {
-    std::string line = "  param = value  # a comment";
-
+protected:
     Settings settings;
+    std::string line = "  param = value  # a comment";
+};
+
+TEST_F(SettingsTest, RemoveStartWhiteSpace)
+{
     settings.removeStartWhitespace(line);
 
     EXPECT_EQ(line, "param = value  # a comment");
+}
+
+TEST_F(SettingsTest, SetParanter)
+{
+    std::string paramName = "re";
+    std::string paramVal = "5";
+
+    settings.setParameter(paramName, paramVal);
+
+    EXPECT_EQ(settings.re, 5);
+}
+
+TEST_F(SettingsTest, ExtractValueString)
+{
+    std::string valueString = settings.extractValueString(line);
+
+    EXPECT_EQ(valueString, "value");
+}
+
+TEST_F(SettingsTest, ExtractParameterName)
+{
+    std::string valueString = settings.extractParameterName(line);
+    EXPECT_EQ(valueString, "");
+
+    line = "param = value  # a comment";
+    valueString = settings.extractParameterName(line);
+
+    EXPECT_EQ(valueString, "param");
 }
