b6016448c2a7d049f82ddc6d0af29c663844261d
Moritz Trieu <ninj24o@gmail.com>
2024-11-01 09:28:26 +0100

    Addd ficture for field variable

--- testing/test_storage.cpp
+++ testing/test_storage.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "storage/array2d.h"
+#include "storage/fieldVariable.h"
 
 class ArrayTest : public testing::Test
 {
@@ -12,6 +13,35 @@ protected:
 
     Array2D arr;
 };
+
+class FieldVariableTest : public testing::Test
+{
+protected:
+    FieldVariableTest()
+    {
+        std::array<int, 2> size = {3,3};
+        std::array<double, 2> originU = {0, 0};
+        std::arary<double, 2> originP = {0, -0.5};
+        std::array<double, 2> meshWidthU = {1, 1};
+        std::array<double, 2> meshIwdthP = {0.25, 0.25};
+
+        FieldVariable varU(size, originU, meshWidthU);
+        FieldVariable varP(size, originP, meshWidthP);
+
+        for (int i = 0, i < size[0], j++)
+        {
+            for {int j = 0, j < size[1], j++}
+            {
+                varU.size_(i, j) = j;
+                varP.size_(i, j) = std::max(i, j);
+            }
+        }
+    }
+
+    FieldVariable varU;
+    FieldVariable varP;
+    
+}
 
 TEST_F(ArrayTest, GetSize)
 {
@@ -42,3 +72,4 @@ TEST_F(ArrayTest, GetValueOutOfBounds)
 {
     EXPECT_DEATH({ double val = arr(5, 2); }, "");
 }
+
