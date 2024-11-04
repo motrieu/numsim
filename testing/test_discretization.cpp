4484069a40f36fbd6a5baf3610711ff333587fa3
Moritz Trieu <ninj24o@gmail.com>
2024-11-04 18:35:08 +0100

    Start CentralDiff unit tests

--- testing/test_discretization.cpp
+++ testing/test_discretization.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <array>
 #include "storage/centralDifferences.h"
+#include <math.h>
 
 class StaggeredGridTest : public testing::Test
 {
@@ -23,14 +24,42 @@ public:
         int iEnd = diff.uIEnd();
         int jBegin = diff.uJBegin();
         int jEnd = diff.uJEnd();
-        for (int i = iBegin - 1)
+
+        for (int i = iBegin - 1; i <= iEnd; i++)
         {
-            for
+            for (int j = jBegin - 1; j <= jEnd; j++)
+            {
+                diff.u(i, j) = std::max(i, j);
+            }
         }
 
         // fill v
+        iBegin = diff.vIBegin();
+        iEnd = diff.vIEnd();
+        jBegin = diff.vJBegin();
+        jEnd = diff.vJEnd();
+
+        for (int i = iBegin - 1; i <= iEnd; i++)
+        {
+            for (int j = jBegin - 1; j <= jEnd; j++)
+            {
+                diff.v(i, j) = std::max(i, j);
+            }
+        }
 
         // fill p
+        iBegin = diff.pIBegin();
+        iEnd = diff.pIEnd();
+        jBegin = diff.pJBegin();
+        jEnd = diff.pJEnd();
+
+        for (int i = iBegin - 1; i <= iEnd; i++)
+        {
+            for (int j = jBegin - 1; j <= jEnd; j++)
+            {
+                diff.p(i, j) = std::max(i, j);
+            }
+        }
     }
 
 protected:
@@ -91,17 +120,65 @@ TEST_F(StaggeredGridTest, TestPJ)
     EXPECT_EQ(jEnd, 4);
 }
 
-// class CentralDifferencesTest : public testing::Test
-// {
-// public:
-//     CentralDifferencesTest() : disc(std::array<int, 2>{4, 4}, std::array<double, 2>{1.0, 1.0})
-//     {
-//         }
+TEST_F(CentralDifferencesTest, Du2DxTest)
+{
+    int iBegin = diff.uIBegin();
+    int iEnd = diff.uIEnd();
 
-// protected:
-//     CentralDifferences disc;
-// };
+    // left border
+    double left = diff.computeDu2Dx(iBegin, 0);
+    // right border
+    double right = diff.computeDu2Dx(iEnd, 0);
+    // middle
+    double middle = diff.computeDu2Dx(2, 0);
 
-// TEST_F(CentralDifferencesTest, TestDu2Dx)
-// {
-// }
+    EXPECT_EQ(left, 2.0);
+    EXPECT_EQ(right, std::pow(2, 2) - std::pow(3.5, 2));
+    EXPECT_EQ(middle, std::pow(2.5, 2) - std::pow(1.5, 2));
+}
+
+TEST_F(CentralDifferencesTest, Dv2DyTest)
+{
+    int jBegin = diff.vJBegin();
+    int jEnd = diff.vJEnd();
+
+    // bottom border
+    double bottom = diff.computeDv2Dy(2, jBegin);
+    // top border
+    double top = diff.computeDv2Dy(2, jEnd);
+
+    EXPECT_EQ(bottom, 0);
+    EXPECT_EQ(top, std::pow(1.5, 2) - std::pow(2.5, 2));
+}
+
+TEST_F(CentralDifferencesTest, DuvDxTest)
+{
+}
+
+TEST_F(CentralDifferencesTest, DuvDyTest)
+{
+}
+
+TEST_F(CentralDifferencesTest, D2uDx2Test)
+{
+}
+
+TEST_F(CentralDifferencesTest, D2uDy2Test)
+{
+}
+
+TEST_F(CentralDifferencesTest, D2vDx2Test)
+{
+}
+
+TEST_F(CentralDifferencesTest, D2vDy2Test)
+{
+}
+
+TEST_F(CentralDifferencesTest, DpDxTest)
+{
+}
+
+TEST_F(CentralDifferencesTest, DpDyTest)
+{
+}
