17c7d405644e0717643da6a800ee5aae0c709fb0
Simon Grether <simon.grether@gmail.com>
2024-11-06 16:38:18 +0100

    update cluster script

--- build/clusterRunScript.sh
+++ build/clusterRunScript.sh
@@ -6,9 +6,7 @@ module load openmpi/3.1.6-gcc-10.2
 module load vtk/9.0.1
 module load cmake/3.18.2
 
-cmake ../..
-cd ..
+cmake -DCMAKE_BUILD_TYPE=Release ..
 make install
-cd src
 
 srun -n 16 ./numsim lid_driven_cavity.txt
