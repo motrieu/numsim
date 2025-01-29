713709f45039320bbafa8d2eaceb7f2ac26833dc
Simon Grether <simon.grether@gmail.com>
2025-01-29 14:59:16 +0100

    correct plot problem for pressure at boundary

--- src/output_writer/output_writer_paraview.cpp
+++ src/output_writer/output_writer_paraview.cpp
@@ -60,12 +60,10 @@ void OutputWriterParaview::writeFile(double currentTime)
     {
       const double x = i*dx;
 
-      if (discretization_->interpolationContainsNoSlip(x, y) == 0)
+      if (!(discretization_->interpolationContainsNoSlip(x, y) == 2))
         arrayPressure->SetValue(index, discretization_->p().interpolateAt(x,y));
-      else if (discretization_->interpolationContainsNoSlip(x, y) == 1)
-        arrayPressure->SetValue(index, 0.0);
       else
-        arrayPressure->SetValue(index, -1.0);
+        arrayPressure->SetValue(index, -1.0e10);
     }
   }
 
